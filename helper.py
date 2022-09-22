fa81e1c796f59c552356ff32d74eea16f393ef3b
maxwell xi <xi@itis.swiss>
2022-09-22 23:15:17 +0200

    add field components averaging functions

--- helper.py
+++ helper.py
@@ -51,3 +51,95 @@ def extract_field_at_line(field, line_orientation, line_index_1, line_index_2):
     field_line = np.array([field_x_line, field_y_line, field_z_line, field_tot_line])
     
     return field_line 
+	
+def x_component_avg(field, avg_square_sidelength_pts, ix, iy, iz):
+    iy_start = iy-int(avg_square_sidelength_pts/2)
+    iy_end = iy+int(avg_square_sidelength_pts/2)
+    iz_start = iz-int(avg_square_sidelength_pts/2)
+    iz_end = iz+int(avg_square_sidelength_pts/2)
+    
+    if iy_start < 0:
+        iy_start = 0
+        print('iy_start falls out of the dataset! iy_start is forced to be 0.')
+    
+    if iy_end > field.shape[2]:
+        iy_end = field.shape[2]
+        print('iy_end falls out of the dataset! iy_end is set to be the largest y index.')
+        
+    
+    if iz_start < 0:
+        iz_start = 0
+        print('iz_start falls out of the dataset! iz_start is forced to be 0.')
+        
+        
+    if iz_end > field.shape[3]:
+        iz_end = field.shape[3]
+        print('iz_end falls out of the dataset! iz_end is set to be the largest z index.')        
+    
+    
+    field_slice = extract_field_at_slice(field, 'x', ix)
+    field_sample = field_slice[0][iy_start:iy_end+1, iz_start:iz_end+1]
+    field_x_avg = np.sum(field_sample)/field_sample.size
+    
+    return field_x_avg
+
+
+def y_component_avg(field, avg_square_sidelength_pts, ix, iy, iz):
+    ix_start = ix-int(avg_square_sidelength_pts/2)
+    ix_end = ix+int(avg_square_sidelength_pts/2)
+    iz_start = iz-int(avg_square_sidelength_pts/2)
+    iz_end = iz+int(avg_square_sidelength_pts/2)
+    
+    if ix_start < 0:
+        ix_start = 0
+        print('ix_start falls out of the dataset! ix_start is forced to be 0.')
+    
+    if ix_end > field.shape[1]:
+        ix_end = field.shape[1]
+        print('ix_end falls out of the dataset! ix_end is set to be the largest x index.')
+        
+    
+    if iz_start < 0:
+        iz_start = 0
+        print('iz_start falls out of the dataset! iz_start is forced to be 0.')
+        
+        
+    if iz_end > field.shape[3]:
+        iz_end = field.shape[3]
+        print('iz_end falls out of the dataset! iz_end is set to be the largest z index.')    
+    
+    field_slice = extract_field_at_slice(field, 'y', iy)
+    field_sample = field_slice[1][ix_start:ix_end+1, iz_start:iz_end+1]
+    field_y_avg = np.sum(field_sample)/field_sample.size
+    
+    return field_y_avg
+
+def z_component_avg(field, avg_square_sidelength_pts, ix, iy, iz):
+    ix_start = ix-int(avg_square_sidelength_pts/2)
+    ix_end = ix+int(avg_square_sidelength_pts/2)
+    iy_start = iy-int(avg_square_sidelength_pts/2)
+    iy_end = iy+int(avg_square_sidelength_pts/2)
+    
+    if ix_start < 0:
+        ix_start = 0
+        print('ix_start falls out of the dataset! ix_start is forced to be 0.')
+    
+    if ix_end > field.shape[1]:
+        ix_end = field.shape[1]
+        print('ix_end falls out of the dataset! ix_end is set to be the largest x index.')
+        
+    
+    if iy_start < 0:
+        iy_start = 0
+        print('iy_start falls out of the dataset! iy_start is forced to be 0.')
+        
+        
+    if iy_end > field.shape[2]:
+        iy_end = field.shape[2]
+        print('iy_end falls out of the dataset! iy_end is set to be the largest y index.')  
+        
+    field_slice = extract_field_at_slice(field, 'z', iz)
+    field_sample = field_slice[2][ix_start:ix_end+1, iy_start:iy_end+1]
+    field_z_avg = np.sum(field_sample)/field_sample.size
+    
+    return field_z_avg
