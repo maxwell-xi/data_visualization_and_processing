797acb86d275e52c327509414cc6f6a5c2b08e4f
maxwell-xi <95143994+maxwell-xi@users.noreply.github.com>
2022-11-15 21:56:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='toolbox',
+    name='data_visualization_and_processing',
     version='0.0.1',
     author='Maxwell XI',
     author_email='xi@itis.swiss',
