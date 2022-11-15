157fa4dc8b39a10622b21ba1cd030d3c05ec28a4
maxwell-xi <95143994+maxwell-xi@users.noreply.github.com>
2022-11-15 22:30:09 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,33 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name='data_visualization_and_processing',
-    version='0.0.1',
-    author='Maxwell XI',
-    author_email='xi@itis.swiss',
+setup(name='data_visualization_and_processing',
+    version='0.1',
     description='Testing installation of Package',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url='https://github.com/maxwell-xi/data_visualization_and_processing',
-    project_urls = {
-        "Bug Tracker": "https://github.com/maxwell-xi/data_visualization_and_processing/issues"
-    },
+    author='MaxwellXI',
+    author_email='xi@itis.swiss',
     license='MIT',
     packages=['data_visualization_and_processing'],
-    install_requires=['requests'],
-)
+    zip_safe=False)
+
+
+#import setuptools
+#
+#with open("README.md", "r", encoding="utf-8") as fh:
+#    long_description = fh.read()
+#
+#setuptools.setup(
+#    name='data_visualization_and_processing',
+#    version='0.0.1',
+#    author='Maxwell XI',
+#    author_email='xi@itis.swiss',
+#    description='Testing installation of Package',
+#    long_description=long_description,
+#    long_description_content_type="text/markdown",
+#    url='https://github.com/maxwell-xi/data_visualization_and_processing',
+#    project_urls = {
+#        "Bug Tracker": "https://github.com/maxwell-xi/data_visualization_and_processing/issues"
+#    },
+#    license='MIT',
+#    packages=['data_visualization_and_processing'],
+#    install_requires=['requests'],
+#)
