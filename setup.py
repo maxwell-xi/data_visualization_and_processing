b54f2f38be0fa1f24312729b0557a3ca803089db
maxwell-xi <95143994+maxwell-xi@users.noreply.github.com>
2022-11-15 21:47:49 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='toolbox',
+    version='0.0.1',
+    author='Maxwell XI',
+    author_email='xi@itis.swiss',
+    description='Testing installation of Package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/maxwell-xi/data_visualization_and_processing',
+    project_urls = {
+        "Bug Tracker": "https://github.com/maxwell-xi/data_visualization_and_processing/issues"
+    },
+    license='MIT',
+    packages=['toolbox'],
+    install_requires=['requests'],
+)
